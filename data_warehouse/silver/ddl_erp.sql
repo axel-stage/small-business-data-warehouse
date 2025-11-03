06ef9746327394917ddab7799fc04dd9c994295b
Axel Stage <dataengineer24@proton.me>
2025-11-03 15:22:33 +0100

    fix: rename attribute and tables

--- data_warehouse/silver/ddl_erp.sql
+++ data_warehouse/silver/ddl_erp.sql
@@ -6,20 +6,20 @@ create schema if not exists silver;
 drop table if exists silver.erp_customer_az12;
 create table silver.erp_customer_az12 (
     cid                     varchar(100),
-    bdate                   date,
+    birthdate               date,
     gender                  varchar(100),
     dwh_create_date         timestamp(2)    default     current_timestamp
 );
 
-drop table if exists silver.erp_loc_a101;
-create table silver.erp_loc_a101 (
+drop table if exists silver.erp_location_a101;
+create table silver.erp_location_a101 (
     cid                     varchar(100),
     country                 varchar(1000),
     dwh_create_date         timestamp(2)    default     current_timestamp
 );
 
-drop table if exists silver.erp_px_cat_g1v2;
-create table silver.erp_px_cat_g1v2 (
+drop table if exists silver.erp_category_g1v2;
+create table silver.erp_category_g1v2 (
     id                      varchar(100),
     category                varchar(100),
     subcategory             varchar(100),
