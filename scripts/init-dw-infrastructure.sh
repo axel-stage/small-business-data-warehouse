a2f3c6fb04e80f44068ef85e4cb7285998756569
Axel Stage <dataengineer24@proton.me>
2025-11-05 19:09:04 +0100

    dev: rework dw infrastructure init
    - add silver layer dll and procudures
    - add gold layer dll and functions

--- scripts/init-dw-infrastructure.sh
+++ scripts/init-dw-infrastructure.sh
@@ -13,5 +13,11 @@ psql -v ON_ERROR_STOP=1 \
 \i ${PWD}/data_warehouse/bronze/ddl_crm.sql
 \i ${PWD}/data_warehouse/bronze/ddl_erp.sql
 \i ${PWD}/data_warehouse/bronze/proc_load_csv.sql
+\i ${PWD}/data_warehouse/silver/ddl_crm.sql
+\i ${PWD}/data_warehouse/silver/ddl_erp.sql
+\i ${PWD}/data_warehouse/silver/proc_etl_crm.sql
+\i ${PWD}/data_warehouse/silver/proc_etl_erp.sql
+\i ${PWD}/data_warehouse/gold/ddl_gold.sql
+\i ${PWD}/data_warehouse/gold/func_gold.sql
 \q
 EOF
